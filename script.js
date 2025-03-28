8f64525e7c5173650db409abd73565af0d81453c
Jonnada Sai Teja <146433770+saitejajonnada@users.noreply.github.com>
2025-03-28 19:27:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function stringChop(str, size) {
-  // your code here
+	if(!str) return [];
+	let chucks = [];
+	for(let i=0;i<str.length;i=i+size){
+		let subStr = str.slice(i,i+size)
+		chucks.push(subStr)
+	}
+	return chucks;
 }
 
 // Do not change the code below
